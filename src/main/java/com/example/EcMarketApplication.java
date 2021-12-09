1f66dde9b2461420a7a68c94c3420cd8cc5761ee
yukimatsunaga <yuki.matsunaga@rakus-partners.co.jp>
2021-12-09 10:40:55 +0900

    first commit

---
+++ src/main/java/com/example/EcMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcMarketApplication.class, args);
+	}
+
+}
